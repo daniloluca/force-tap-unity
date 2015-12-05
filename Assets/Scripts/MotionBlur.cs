c68d514a71be85f714771f84486d4e2585589fd9
daniloluca <danilolucasmd@gmail.com>
2015-12-05 04:21:58 -0200

    Adicionado Trail Effect (motion blur) ao personagem, no level 1.

---
+++ Assets/Scripts/MotionBlur.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionBlur : MonoBehaviour {
+
+	public GameObject trail;
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("deadDelay");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			
+	}
+
+	IEnumerator deadDelay(){
+
+		trail.GetComponent<SpriteRenderer>().sprite = transform.GetComponent<SpriteRenderer>().sprite;
+		trail.GetComponent<SpriteRenderer>().color = transform.GetComponent<SpriteRenderer>().color;
+		trail.GetComponent<SpriteRenderer>().material = transform.GetComponent<SpriteRenderer>().material;
+		trail.GetComponent<SpriteRenderer>().enabled = transform.GetComponent<SpriteRenderer>().enabled;
+		
+		Instantiate(trail, new Vector3(transform.position.x, transform.position.y, transform.position.z+1), Quaternion.identity);
+
+		yield return new WaitForSeconds(speed);
+		StartCoroutine("deadDelay");
+	}
+}
---
+++ Assets/Scripts/TrailTransparency.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailTransparency : MonoBehaviour {
+
+	public float speed = 1f;
+	private Color color;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("deadDelay");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(transform.GetComponent<SpriteRenderer>().color.a <= 0){
+			DestroyObject(gameObject);
+		}
+
+		color = transform.GetComponent<SpriteRenderer>().color;
+
+		color.a-=Time.deltaTime/speed;
+
+		transform.GetComponent<SpriteRenderer>().color = color;
+			
+	}
+
+	IEnumerator deadDelay(){
+
+		
+
+		yield return new WaitForSeconds(speed);
+		StartCoroutine("deadDelay");
+	}
+}

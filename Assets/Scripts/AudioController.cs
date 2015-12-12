1955fb28de64b86eccec5f5c67233a85bde16964
daniloluca <danilolucasmd@gmail.com>
2015-12-12 03:58:14 -0200

    Implementado Pause, e Menu Pause

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class AudioController : MonoBehaviour {
 
+	private bool destroy = true;
+
+	void OnLevelWasLoaded(){
+		if(destroy){
+			DestroyObject(this.gameObject);
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad(this.gameObject);
+		destroy = false;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+
+	public GameObject pausePanel;
+
+	public void pause(){
+		Time.timeScale = 0;
+		pausePanel.SetActive(true);
+	}
+
+	public void unPause(){
+		Time.timeScale = 1;
+		pausePanel.SetActive(false);
+	}
+}
